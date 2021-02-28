cf7d204c47ec801a98bb4d920f9e6f1c9958cc9b
Leon Cresdee <leon@cresdee.com>
2021-02-28 16:28:03 +0000

    Added shader

--- js/sdf.js
+++ js/sdf.js
@@ -1,7 +1,19 @@
 
+let sdfShader;
 
+function preload(){
+    sdfShader = loadShader('../glsl/sdf.vert', '../glsl/sdf.frag');
+
+}
 
 function setup(){
+    createCanvas(500, 500);
     background(0);
+    shader(sdfShader);
+    noStroke();
 
+}
+
+function draw(){
+    quad(-1, -1, 1, -1, 1, 1, -1, 1);
 }
